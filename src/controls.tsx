f7c100ce0652227b29f5c9e9e9b8a5bdd7cf4f3e
Péter Pallos <pallosp@gmail.com>
2025-05-21 17:55:41 +0200

    Move the plot controls to a separate file

---
+++ src/controls.tsx
@@ -0,0 +1,61 @@
+import {Button, Checkbox, FormControlLabel, TextField, Typography} from '@mui/material';
+import {useEffect, useRef} from 'preact/hooks';
+
+export function FunctionButton(props: {text: string; selected: boolean; onclick: () => void}) {
+  return (
+    <Button variant={props.selected ? 'contained' : 'outlined'} onClick={props.onclick}>
+      <Typography sx={{textTransform: 'none', whiteSpace: 'nowrap'}}>{props.text}</Typography>
+    </Button>
+  );
+}
+
+export function ShowEdgesCheckbox(props: {setShowEdges: (checked: boolean) => void}) {
+  return (
+    <FormControlLabel
+      control={
+        <Checkbox
+          sx={{height: '36px'}}
+          onChange={(e) => props.setShowEdges((e.currentTarget as HTMLInputElement).checked)}
+        />
+      }
+      label="Show edges"
+      sx={{margin: 0}}
+    />
+  );
+}
+
+export function PixelSizeInput(props: {
+  pixelSizeExponent: number;
+  setPixelSizeExponent: (size: number) => void;
+}) {
+  const inputRef = useRef<HTMLInputElement>();
+  useEffect(() => {
+    const input = inputRef.current;
+    input.onchange = () => {
+      let size = +input.value;
+      if (size < -2 || size > 9 || !Number.isInteger(size)) {
+        size = props.pixelSizeExponent;
+      }
+      props.setPixelSizeExponent(size);
+      this.forceUpdate();
+    };
+  });
+
+  return (
+    <FormControlLabel
+      label="Pixel size: 2^"
+      labelPlacement="start"
+      control={
+        <TextField
+          inputRef={inputRef}
+          type="number"
+          value={props.pixelSizeExponent}
+          variant="outlined"
+          slotProps={{htmlInput: {min: -2, max: 9, step: 1}}}
+          sx={{'& .MuiInputBase-input': {height: '36px', boxSizing: 'border-box'}}}
+        />
+      }
+      sx={{margin: 0}}
+    ></FormControlLabel>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,8 @@
-import {
-  Button,
-  ButtonGroup,
-  Checkbox,
-  FormControlLabel,
-  TextField,
-  Typography
-} from '@mui/material';
+import {ButtonGroup} from '@mui/material';
 import {render} from 'preact';
-import {useEffect, useRef, useState} from 'preact/hooks';
+import {useState} from 'preact/hooks';
 
+import {FunctionButton, PixelSizeInput, ShowEdgesCheckbox} from './controls';
 import {PanZoom} from './pan_zoom';
 import {circlePlot, linePlot, mandelbrotPlot, sinCosPlot} from './plot_configs';
 import {PlotStats} from './plot_stats';
@@ -80,65 +74,6 @@ export function App() {
         <PlotStats stats={stats} />
       </footer>
     </>
-  );
-}
-
-function FunctionButton(props: {text: string; selected: boolean; onclick: () => void}) {
-  return (
-    <Button variant={props.selected ? 'contained' : 'outlined'} onClick={props.onclick}>
-      <Typography sx={{textTransform: 'none', whiteSpace: 'nowrap'}}>{props.text}</Typography>
-    </Button>
-  );
-}
-
-function ShowEdgesCheckbox(props: {setShowEdges: (checked: boolean) => void}) {
-  return (
-    <FormControlLabel
-      control={
-        <Checkbox
-          sx={{height: '36px'}}
-          onChange={(e) => props.setShowEdges((e.currentTarget as HTMLInputElement).checked)}
-        />
-      }
-      label="Show edges"
-      sx={{margin: 0}}
-    />
-  );
-}
-
-function PixelSizeInput(props: {
-  pixelSizeExponent: number;
-  setPixelSizeExponent: (size: number) => void;
-}) {
-  const inputRef = useRef<HTMLInputElement>();
-  useEffect(() => {
-    const input = inputRef.current;
-    input.onchange = () => {
-      let size = +input.value;
-      if (size < -2 || size > 9 || !Number.isInteger(size)) {
-        size = props.pixelSizeExponent;
-      }
-      props.setPixelSizeExponent(size);
-      this.forceUpdate();
-    };
-  });
-
-  return (
-    <FormControlLabel
-      label="Pixel size: 2^"
-      labelPlacement="start"
-      control={
-        <TextField
-          inputRef={inputRef}
-          type="number"
-          value={props.pixelSizeExponent}
-          variant="outlined"
-          slotProps={{htmlInput: {min: -2, max: 9, step: 1}}}
-          sx={{'& .MuiInputBase-input': {height: '36px', boxSizing: 'border-box'}}}
-        />
-      }
-      sx={{margin: 0}}
-    ></FormControlLabel>
   );
 }
 
